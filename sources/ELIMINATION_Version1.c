4cd49eb28b04eb18d8873cef57971ae3a4e21f3e
mlebout <mlebout@etudiant.univ-rennes1.fr>
2024-01-08 13:08:43 +0100

    Ajout de la mesure du temps CPU.

--- sources/ELIMINATION_Version1.c
+++ sources/ELIMINATION_Version1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <time.h>
 
 #include "../includes/defines.h"
 #include "../includes/types1.h"
@@ -14,6 +15,8 @@ int main()
     t_grille1 grille;
     int nbCasesVide;
     bool progression = true;
+    clock_t clockDebut, clockFin;
+    double duree;
 
     bool grilleCharge = false;
     while (!grilleCharge)
@@ -28,12 +31,13 @@ int main()
 
     initialiserCandidats(grille);
 
+    clockDebut = clock();
     while ((nbCasesVide > 0) && progression)
     {
         progression = false;
 
         singletonNu(grille, &nbCasesVide, &progression);
-        //singletonCache(grille, &nbCasesVide, &progression);
+        singletonCache(grille, &nbCasesVide, &progression);
 
         if (progression)
         {
@@ -44,9 +48,12 @@ int main()
             printf("false\n");
         }
     }
+    clockFin = clock();
+    duree = (double) (clockFin - clockDebut) / CLOCKS_PER_SEC;
 
     afficherGrille(grille);
     printf("%d cases vides\n", nbCasesVide);
+    printf("Temps : %f s\n", duree);
 
     return EXIT_SUCCESS;
 }
