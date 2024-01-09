6bf8db442ed4588865bcab86133cdab79a6a9805
mlebout <mlebout@etudiant.univ-rennes1.fr>
2024-01-09 11:32:09 +0100

    Création des fichiers procedures_backtracking.c et .h et déplacement de la fonction backtracking().

---
+++ includes/procedures_backtracking.h
@@ -0,0 +1,12 @@
+#ifndef PRCEDURES_BACKTRACKING_H
+    #define PRCEDURES_BACKTRACKING_H
+
+    #include <stdbool.h>
+
+    #include "types_backtracking.h"
+
+
+    bool backtracking(t_grille_backtracking grille, int numeroCase);
+
+#endif
+
--- sources/backtracking.c
+++ sources/backtracking.c
@@ -4,6 +4,7 @@
 #include <time.h>
 
 #include "../includes/types_backtracking.h"
+#include "../includes/procedures_backtracking.h"
 
 
 int main()
@@ -45,56 +46,5 @@ int main()
     }
 
     return EXIT_SUCCESS;
-}
-
-
-bool backtracking(t_grille_backtracking grille, int numeroCase)
-{
-    bool result;
-    int ligne, colonne;
-
-    result = false;
-
-    if (numeroCase == TAILLE_GRILLE * TAILLE_GRILLE)
-    {
-        // On a traité toutes les cases, la grille est résolue
-        result = true;
-    }
-    else
-    {
-        // On récupère les "coordonnées" de la case
-        ligne = numeroCase / TAILLE_GRILLE;
-        colonne = numeroCase % TAILLE_GRILLE;
-        if (grille[ligne][colonne].valeur != 0)
-        {
-            // Si la case n’est pas vide, on passe à la suivante
-            // (appel récursif)
-            result = backtracking(grille, numeroCase + 1);
-        }
-        else
-        {
-            for (int valeur = 1; valeur <= TAILLE_GRILLE; valeur++)
-            {
-                if (possible())
-                {
-                    // Si la valeur est autorisée on l'inscrit
-                    //dans la case...
-                    grille[ligne][colonne].valeur = valeur;
-                    // ... et on passe à la suivante : appel récursif
-                    // pour voir si ce choix est bon par la suite
-                    if (backtracking(grille, numeroCase + 1))
-                    {
-                        result = true;
-                    }
-                    else
-                    {
-                        grille[ligne][colonne].valeur = 0;
-                    }
-                }
-            }
-        }
-    }
-
-    return result;
 }
 
