786fd71528f13ee2aea3cc81a130380604312e1a
mlebout <mlebout@etudiant.univ-rennes1.fr>
2024-01-19 15:12:35 +0100

    Ajout du fichier procedures.h

---
+++ includes/procedures.h
@@ -0,0 +1,21 @@
+#ifndef PROCEDURES_H
+    #define PROCEDURES_H
+
+    #include <stdbool.h>
+
+    #include "types.h"
+
+
+    bool chargerGrille(grille_t grille);
+    void initialiserCandidats(grille_t grille);
+    int getNbCaseVides(grille_t grille);
+    void ajouterCandidat(case_t *laCase, int valeur);
+    void retirerCandidat(case_t *laCase, int valeur);
+    void retirerTousLesCandidat(case_t *laCase);
+    bool estCandidat(case_t *laCase, int valeur);
+    int nbCandidats(case_t *laCase);
+    void afficherGrille(grille_t grille);
+    bool possible(grille_t grille, int numLigne, int numColone, int valeur);
+
+#endif
+
