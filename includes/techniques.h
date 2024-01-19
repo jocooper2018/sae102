4a4c664fd0798070fdd3295425c4b5b2d95cbfa0
mlebout <mlebout@etudiant.univ-rennes1.fr>
2024-01-19 15:15:19 +0100

    Ajout du fichier techniques.h

---
+++ includes/techniques.h
@@ -0,0 +1,16 @@
+#ifndef TECHNIQUES_H
+    #define TECHNIQUES_H
+
+    #include "types.h"
+
+
+    void singletonNu(grille_t grille, int *nbCasesVide, bool *progression);
+    void singletonCache(grille_t grille, int *nbCasesVide, bool *progression);
+    void accepterCandidat(grille_t grille, int numLigne, int numCol, int candidat);
+
+    int nbOccurenceCandidatLigne(grille_t grille, int numLigne, int candidat);
+    int nbOccurenceCandidatColonne(grille_t grille, int numCol, int candidat);
+    int nbOccurenceCandidatBlock(grille_t grille, int numLigne, int numCol, int candidat);
+
+#endif
+
